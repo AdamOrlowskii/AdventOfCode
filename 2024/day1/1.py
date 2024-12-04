8792683e3523d60a0f4b9a1b0d104ff06d11aa23
Adam Orłowski <131544697+AdamOrlowskii@users.noreply.github.com>
2024-12-04 18:37:23 +0100

    Add files via upload

---
+++ 2024/day1/1.py
@@ -0,0 +1,20 @@
+def count():
+    list1 = []
+    list2 = []
+    answer = 0
+    file = open("data.txt", "r")
+    for line in file:
+        column1, column2 = line.split()
+        list1.append(column1)
+        list2.append(column2)
+    list1.sort()
+    list2.sort()
+    for x in range(len(list1)):
+        list1[x] = int(list1[x])
+        list2[x] = int(list2[x])
+        answer += abs(list1[x] - list2[x])
+    print(answer)
+
+
+if __name__ == '__main__':
+    count()
